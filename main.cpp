e5ecf229b75ec0125d1ff5bcde5885d3a234a5c8
GiulioUmbrella <giulio.umbrella@gmail.com>
2024-03-06 15:07:16 +0100

    Add Filter handle struct
    struct  to handle minifilter driver

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <fltKernel.l>
+#include <dontuse.h>
+#include <suppress.h>
+
+
+/**
+*  A structure to the filter handle
+*/
+
+typedef struct _NULL_FILTER_DATA{
+
+    PFLT_FILTER FilterHandle;
+} NULL_FILTER_DATA, *PNULL_FILTER_DATA;
+
+
+NULL_FILTER_DATA NullFilterData;
+
